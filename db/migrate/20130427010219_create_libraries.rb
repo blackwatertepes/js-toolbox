b37f498bb31a8296176c920aaa16becb60405cd9
Tyler Kuhn <tyler@wargoats.com>
2013-04-27 16:52:10 -0700

    added last commited datetime

--- db/migrate/20130427010219_create_libraries.rb
+++ db/migrate/20130427010219_create_libraries.rb
@@ -1,13 +1,14 @@
 class CreateLibraries < ActiveRecord::Migration
   def change
     create_table :libraries do |t|
+      t.integer :github_id
       t.string :name
       t.string :author
       t.text :description
       t.string :website_url
       t.string :doc_url
-      t.string :github_url
       t.references :category
+      t.datetime :pushed_at
 
       t.timestamps
     end
