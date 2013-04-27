f2cbe77c138446f7dbecd2156c3fe824bb483434
Tyler Kuhn <tyler@wargoats.com>
2013-04-26 17:23:49 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trending::Application.config.secret_token = '59a010fc6c8bd32bd578339e19d698a0aa74a7e193349b648fac0487491c23f08e1d585a1b90437d3c58e02ef71d7fcf8631d26fdec7b999d8ff4817747f1fdc'
