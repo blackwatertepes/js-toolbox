2774bde63b1894bc5e52e67659324a19fe518f0c
Tyler Kuhn <tyler@wargoats.com>
2013-04-28 01:29:36 -0700

    added utilities libraries

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,59 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.delete_all
-Library.delete_all
+# Category.delete_all
+# Library.delete_all
+# 
+# cat_framework = Category.create(name: 'Frameworks')
+# 
+# [ {github_id: 952189, author: 'documentcloud', name: 'backbone', 
+#     description: "Give your JS App some Backbone with Models, Views, Collections, and Events",
+#     website_url: "http://backbonejs.org",
+#     doc_url: "http://backbonejs.org"}, 
+#   {github_id: 1801829, author: 'emberjs', name: 'ember.js',
+#     description: "Ember.js - A JavaScript framework for creating ambitious web applications",
+#     website_url: "http://www.emberjs.com",
+#     doc_url: "http://emberjs.com/guides/"},
+#   {github_id: 757363, author: 'stevesanderson', name: 'knockout',
+#     description: "Knockout makes it easier to create rich, responsive UIs with JavaScript",
+#     website_url: "http://knockoutjs.com",
+#     doc_url: "http://knockoutjs.com/documentation/introduction.html"},
+#   {github_id: 460078, author: 'angular', name: 'angular.js',
+#     description: "",
+#     website_url: "http://angularjs.org",
+#     doc_url: "http://docs.angularjs.org/guide/"},
+#   {github_id: 1540246, author: 'spine', name: 'spine',
+#     description: "Lightweight MVC library for building JavaScript applications",
+#     website_url: "http://spinejs.com",
+#     doc_url: "http://spinejs.com/docs"},
+#   {github_id: 85670, author: 'yui', name: 'yui3',
+#     description: "YUI 3.x Source Tree",
+#     website_url: "http://yuilibrary.com",
+#     doc_url: "http://yuilibrary.com/yui/docs/"},
+#   {github_id: 1931485, author: 'arturadib', name: 'agility',
+#     description: "Javascript MVC for the \"write less, do more\" programmer",
+#     website_url: "http://agilityjs.com",
+#     doc_url: "http://agilityjs.com/docs/docs.html"},
+#   {github_id: 233888, author: 'dojo', name: 'dojo',
+#     description: "The official read-only mirror of Dojo Core.",
+#     website_url: "http://dojotoolkit.org",
+#     doc_url: "http://dojotoolkit.org/documentation/"},
+#   {github_id: 77544, author: 'thatcher', name: 'jquery-claypool',
+#     description: "Claypool is a small, fast, railable Javascript Application Framework  , built on jQuery that provides all the usual important patterns for large, long-lived client-side apps, server-side apps, or something strangely, beautifully in the middle.",
+#     website_url: "http://claypooljs.com",
+#     doc_url: "http://docs.jquery.com/Plugins/Claypool"}].each do |library|
+#     Library.create(library.merge({category_id: cat_framework.id}))
+#   end
 
-cat_framework = Category.create(name: 'Frameworks')
+cat_utility = Category.create(name: 'Utility')
 
-[ {github_id: 952189, author: 'documentcloud', name: 'backbone', 
-    description: "Give your JS App some Backbone with Models, Views, Collections, and Events",
-    website_url: "http://backbonejs.org",
-    doc_url: "http://backbonejs.org"}, 
-  {github_id: 1801829, author: 'emberjs', name: 'ember.js',
-    description: "Ember.js - A JavaScript framework for creating ambitious web applications",
-    website_url: "http://www.emberjs.com",
-    doc_url: "http://emberjs.com/guides/"},
-  {github_id: 757363, author: 'stevesanderson', name: 'knockout',
-    description: "Knockout makes it easier to create rich, responsive UIs with JavaScript",
-    website_url: "http://knockoutjs.com",
-    doc_url: "http://knockoutjs.com/documentation/introduction.html"},
-  {github_id: 460078, author: 'angular', name: 'angular.js',
-    description: "",
-    website_url: "http://angularjs.org",
-    doc_url: "http://docs.angularjs.org/guide/"},
-  {github_id: 1540246, author: 'spine', name: 'spine',
-    description: "Lightweight MVC library for building JavaScript applications",
-    website_url: "http://spinejs.com",
-    doc_url: "http://spinejs.com/docs"},
-  {github_id: 85670, author: 'yui', name: 'yui3',
-    description: "YUI 3.x Source Tree",
-    website_url: "http://yuilibrary.com",
-    doc_url: "http://yuilibrary.com/yui/docs/"},
-  {github_id: 1931485, author: 'arturadib', name: 'agility',
-    description: "Javascript MVC for the \"write less, do more\" programmer",
-    website_url: "http://agilityjs.com",
-    doc_url: "http://agilityjs.com/docs/docs.html"},
-  {github_id: 233888, author: 'dojo', name: 'dojo',
-    description: "The official read-only mirror of Dojo Core.",
-    website_url: "http://dojotoolkit.org",
-    doc_url: "http://dojotoolkit.org/documentation/"},
-  {github_id: 77544, author: 'thatcher', name: 'jquery-claypool',
-    description: "Claypool is a small, fast, railable Javascript Application Framework  , built on jQuery that provides all the usual important patterns for large, long-lived client-side apps, server-side apps, or something strangely, beautifully in the middle.",
-    website_url: "http://claypooljs.com",
-    doc_url: "http://docs.jquery.com/Plugins/Claypool"}].each do |library|
-    Library.create(library.merge({category_id: cat_framework.id}))
-  end
+[ {github_id: 349241, author: 'documentcloud', name: 'underscore',
+    description: "JavaScript's utility _ belt",
+    website_url: "http://underscorejs.org",
+    doc_url: "http://underscorejs.org"},
+  {github_id: 3955647, author: 'bestiejs', name: 'lodash',
+    description: "A low-level utility library delivering consistency, customization, performance, and extra features.",
+    website_url: "http://lodash.com",
+    doc_url: "http://lodash.com"}].each do |library|
+  Library.create(library.merge({category_id: cat_utility.id}))
+end
--- lib/tasks/status.rake
+++ lib/tasks/status.rake
@@ -1,23 +1,40 @@
 namespace :status do
-  task get: :environment do
-    url = URI.parse("")
-    conn = Faraday.new :url => 'https://api.github.com', ssl: {verify: false} do |c|
-      c.use Faraday::Request::UrlEncoded  # encode request params as "www-form-urlencoded"
-      c.use Faraday::Response::Logger     # log request & response to STDOUT
-      c.use Faraday::Adapter::NetHttp     # perform requests with Net::HTTP
-    end
+  task frameworks: :environment do
+    conn = establish_connection
 
-    Library.all.each do |library|
-      lib_url = "/repos/#{library.author}/#{library.name}"
-      response = conn.get lib_url
-      data = JSON.parse(response.body)
-      params = {watchers: data["watchers"], issues: data["open_issues"], forks: data["forks"]}
-       
-      # response = conn.get "#{lib_url}/downloads"
-      # data = JSON.parse(response.body)
-      # params.merge({downloads: data.last["download_count"]}) if data.last
-      
-      Status.create(params.merge({library_id: library.id}))
+    Category.find_by_name("frameworks").libraries.each do |library|
+      create_library(library, conn)
     end
   end
+  
+  task utilities: :environment do
+    conn = establish_connection
+    
+    Category.find_by_name("utilities").libraries.each do |library|
+      create_library(library, conn)
+    end
+  end
+end
+
+def establish_connection
+  url = URI.parse("")
+  conn = Faraday.new :url => 'https://api.github.com', ssl: {verify: false} do |c|
+    c.use Faraday::Request::UrlEncoded  # encode request params as "www-form-urlencoded"
+    c.use Faraday::Response::Logger     # log request & response to STDOUT
+    c.use Faraday::Adapter::NetHttp     # perform requests with Net::HTTP
+  end
+  return conn
+end
+
+def create_library(library, conn)
+  lib_url = "/repos/#{library.author}/#{library.name}"
+  response = conn.get lib_url
+  data = JSON.parse(response.body)
+  params = {watchers: data["watchers"], issues: data["open_issues"], forks: data["forks"]}
+   
+  # response = conn.get "#{lib_url}/downloads"
+  # data = JSON.parse(response.body)
+  # params.merge({downloads: data.last["download_count"]}) if data.last
+  
+  Status.create(params.merge({library_id: library.id}))
 end
