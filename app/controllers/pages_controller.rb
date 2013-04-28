0024183d49ceb45e0910f8c327853a98994ec266
Tyler Kuhn <tyler@wargoats.com>
2013-04-28 02:04:20 -0700

    added page titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   def index
     @categories = Category.all
+    @page = "Home"
   end
   
   def category
     @category = Category.includes(:libraries => :statuses).find_by_name(params[:name])
     @libraries = @category.libraries
+    @page = @category.name.capitalize
   end
 end
