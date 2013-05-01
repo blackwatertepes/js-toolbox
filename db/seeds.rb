f2a44e1cb628eb7c07da80024558748cac7ea3f8
Tyler Kuhn <tyler@wargoats.com>
2013-05-01 12:18:59 -0700

    added testing libraries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,16 @@
-cat = Category.create(name: 'mobile', full_name: 'Mobile')
+cat = Category.create(name: 'testing', full_name: 'Testing')
 
-[ {github_id: 907410, author: 'jquery', name: 'jquery-mobile',
-      description: "jQuery Mobile Framework",
-      website_url: "http://jquerymobile.com/",
-      doc_url: "http://api.jquerymobile.com/"},
-  {github_id: 1171324, author: 'h5bp', name: 'mobile-boilerplate',
-        description: "A front-end template that helps you build fast, modern mobile web apps.",
-        website_url: "http://mobileboilerplate.com",
-        doc_url: "https://github.com/h5bp/mobile-boilerplate/blob/v4.1.0/doc/README.md"},
-  {github_id: 745502, author: 'senchalabs', name: 'jQTouch',
-        description: "Create powerful mobile apps with just HTML, CSS, and Zepto.js (or jQuery).",
-        website_url: "http://jqtjs.com/",
-        doc_url: "https://github.com/senchalabs/jQTouch/wiki"},
-  {github_id: 798217, author: 'cpojer', name: 'mootools-mobile',
-        description: "Makes your web applications more touching.",
-        website_url: "http://cpojer.net/MooTools/mobile/Demos/",
-        doc_url: "http://cpojer.net/MooTools/mobile/Demos/"}].each do |library|
+[ {github_id: 84009, author: 'pivotal', name: 'jasmine',
+      description: "DOM-less simple JavaScript testing framework",
+      website_url: "http://pivotal.github.io/jasmine/",
+      doc_url: "http://pivotal.github.io/jasmine/"},
+  {github_id: 1451352, author: 'visionmedia', name: 'mocha',
+        description: "mocha - simple, flexible, fun javascript test framework for node.js & the browser. (BDD, TDD, QUnit styles via interfaces)",
+        website_url: "http://visionmedia.github.io/mocha/",
+        doc_url: "http://visionmedia.github.io/mocha/"},
+  {github_id: 259691, author: 'jquery', name: 'qunit',
+        description: "An easy-to-use JavaScript Unit Testing framework.",
+        website_url: "http://qunitjs.com/",
+        doc_url: "http://api.qunitjs.com/"}].each do |library|
   Library.create(library.merge({category_id: cat.id}))
 end
